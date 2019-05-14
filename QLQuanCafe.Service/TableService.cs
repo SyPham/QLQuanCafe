55b1aa2dfb1bcc342a1433780c283c9e61b8f949
Dell-Henry <phamtiensy8696@gmail.com>
2019-05-15 00:09:30 +0700

    WebApi TableController

--- QLQuanCafe.Service/TableService.cs
+++ QLQuanCafe.Service/TableService.cs
@@ -11,14 +11,12 @@ namespace QLQuanCafe.Service
 {
     public interface ITableService
     {
-<<<<<<< HEAD
+
         //void Add(Table table);
         Table Add(Table table);
         void Delete(int id);
-=======
-        void Add(Table table);
         void Delete(Table table);
->>>>>>> 5e2fe6c58a334e0d0325e2c28574de322a3778c7
+
         void Update(Table table);
         IEnumerable<Table> GetAll();
         IEnumerable<Table> GetAllPaging(int page, int pageSize, out int totalRow);
@@ -38,25 +36,17 @@ namespace QLQuanCafe.Service
             _unitOfWork = unitOfWork;
         }
 
-<<<<<<< HEAD
+
         public Table Add(Table table)
         {
-           return _tableRepository.Add(table);
+            return _tableRepository.Add(table);
         }
 
-        public void Delete(int id)
-        {
-            _tableRepository.Delete(id);
-=======
-        public void Add(Table table)
-        {
-            _tableRepository.Add(table);
-        }
 
         public void Delete(Table table)
         {
             _tableRepository.Delete(table);
->>>>>>> 5e2fe6c58a334e0d0325e2c28574de322a3778c7
+
         }
 
         public IEnumerable<Table> GetAll()
@@ -89,26 +79,21 @@ namespace QLQuanCafe.Service
             return _tableRepository.GetSingleById(id);
         }
 
-<<<<<<< HEAD
-
         public void Update(Table table)
         {
             _tableRepository.Update(table);
+        }
+        public void Delete(int id)
+        {
+            _tableRepository.Delete(id);
         }
 
         public void SaveChange()
         {
             _unitOfWork.Commit();
-=======
-        public void SaveChange()
-        {
-            _unitOfWork.Commit();
+
         }
 
-        public void Update(Table table)
-        {
-            _tableRepository.Update(table);
->>>>>>> 5e2fe6c58a334e0d0325e2c28574de322a3778c7
-        }
+
     }
 }
